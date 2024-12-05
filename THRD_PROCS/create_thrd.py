2dce8048e1de0878391e2cc44043947322734bda
Levoluti0n <levon.mezhlumyan.h@gmail.com>
2024-12-05 14:12:32 +0400

    ADD Threads, Processes

---
+++ THRD_PROCS/create_thrd.py
@@ -0,0 +1,11 @@
+import time
+from threading import Thread
+
+def do_work(id: int):
+    print(f"Starting Job: {id}")
+    time.sleep(1)
+    print(f"Finished Job : {id}")
+
+for i in range(5):
+    thrd = Thread(target=do_work, args=(i,))
+    thrd.start()
