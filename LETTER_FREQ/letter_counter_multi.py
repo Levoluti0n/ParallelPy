44fe5510fea71e89e2e7e651535e733eb0b8b7f8
Levoluti0n <levon.mezhlumyan.h@gmail.com>
2024-12-05 15:00:19 +0400

    Handle Race Condition

--- LETTER_FREQ/letter_counter_multi.py
+++ LETTER_FREQ/letter_counter_multi.py
@@ -1,29 +1,30 @@
-# With Race Condition
-
 import time
 import json
 import urllib.request
-from threading import Thread
+from threading import Thread, Lock
 
 
-def count_letters(url, frequency):
+def count_letters(url: str, frequency: dict, mutex: Lock):
     response = urllib.request.urlopen(url)
     text = str(response.read())
+    mutex.acquire()
     for l in text:
         letter = l.lower()
         if letter.isalpha():
             frequency[letter] = frequency.get(letter, 0) + 1
+    mutex.release()
 
 
 def main():
     frequency = {}
+    mutex = Lock()
 
     start = time.time()
     threads = []
     for i in range(1000, 1020):
         thread = Thread(target=count_letters,
                         args=(f"https://www.rfc-editor.org/rfc/rfc{i}.txt",
-                              frequency))
+                              frequency, mutex))
         threads.append(thread)
         thread.start()
     for thread in threads:
